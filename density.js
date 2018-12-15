e3ade9639289503b2c8907b746c071c4be2d6e78
Shi Chong <sz329@cornell.edu>
2018-12-15 07:59:14 -0500

    eliiminated duplicate code by using a helper function

--- density.js
+++ density.js
@@ -111,6 +111,33 @@ exports.facilityList = function (req, res) {
 
 exports.facilityInfo = function (req, res) {
     const query = datastore.createQuery('hours');
+    
+    function getFacilityInfo(facility){
+        if (facility && facility['operatingHours']) {
+            const nextClosing = facility['operatingHours'].find((e) => e.startTimestamp < date && e.endTimestamp > date);
+            let nextOpen = -1;
+            let closingAt;
+
+            if (nextClosing) {
+                closingAt = nextClosing.endTimestamp;
+            } else {
+                closingAt = -1;
+
+                // TODO This is broken. Needs to find last opening, not first opening.
+                nextOpen = facility['operatingHours'].find((e) => e.startTimestamp > date) || {};
+            }
+
+            return ({
+                id: facility.id,
+                campusLocation: facility.campusLocation,
+                nextOpen: nextOpen.startTimestamp || -1,
+                closingAt: closingAt,
+                dailyHours: facility.operatingHours
+            })
+        } else {
+            return null;
+        }
+    }
 
     datastore.runQuery(query, (error, hours) => {
         if (error) {
@@ -123,33 +150,15 @@ exports.facilityInfo = function (req, res) {
         if (req.query.id) {
             const id = req.query.id;
             if (id in ID_MAP) {
-                let h = hours.find(x => x.id == id);
+                let facility = hours.find(x => x.id == id);
+                let facilityInfo = getFacilityInfo(facility);
 
-                if (h && h['operatingHours']) {
-                    const nextClosing = h['operatingHours'].find((e) => e.startTimestamp < date && e.endTimestamp > date);
-                    let nextOpen = -1;
-                    let val;
-
-                    if (nextClosing) {
-                        val = nextClosing.endTimestamp;
-                    } else {
-                        val = -1;
-
-                        // TODO
-                        nextOpen = h['operatingHours'].find((e) => e.startTimestamp > date) || {};
-                    }
-
-                    res.status(200).send([{
-                        id: h.id,
-                        campusLocation: h['campusLocation'],
-                        nextOpen: nextOpen.startTimestamp || -1,
-                        closingAt: val,
-                        dailyHours: h['operatingHours']
-                    }]);
-                } else {
+                if (facilityInfo == null){
                     res.status(401).send(JSON.stringify({
                         error: `Invalid ID (access): ${id}`
-                    }));
+                    }));                   
+                } else {
+                    res.status(200).send([facilityInfo]);                   
                 }
                 return;
             } else {
@@ -160,33 +169,9 @@ exports.facilityInfo = function (req, res) {
             }
         }
 
-
         res.status(200).send(Object.keys(ID_MAP).map((id) => {
-            let h = hours.find(x => x.id == id);
-            if (h && h['operatingHours']) {
-                const nextClosing = h['operatingHours'].find((e) => e.startTimestamp < date && e.endTimestamp > date);
-                let nextOpen = -1;
-                let val;
-
-                if (nextClosing) {
-                    val = nextClosing.endTimestamp;
-                } else {
-                    val = -1;
-
-                    // TODO This is broken. Needs to find last opening, not first opening.
-                    nextOpen = h['operatingHours'].find((e) => e.startTimestamp > date) || {};
-                }
-
-                return ({
-                    id: h.id,
-                    campusLocation: h.campusLocation,
-                    nextOpen: nextOpen.startTimestamp || -1,
-                    closingAt: val,
-                    dailyHours: h.operatingHours
-                })
-            } else {
-                return null;
-            }
+            let facility = hours.find(x => x.id == id);
+            getFacilityInfo(facility);
         }));
     });
 }
