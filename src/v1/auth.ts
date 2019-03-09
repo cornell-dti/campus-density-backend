d2c1abb2731d852087b57f3df2b8b5c47722f252
ad665 <ad665@cornell.edu>
2019-03-09 17:44:54 -0500

    cleaned up code and changed databaseURL to be a .env variable

--- src/v1/auth.ts
+++ src/v1/auth.ts
@@ -17,28 +17,17 @@
 
 import * as express from 'express';
 import * as admin from 'firebase-admin';
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const Datastore = require('@google-cloud/datastore');
-
-const datastore = Datastore();
-
-/* eslint-enable */
-
 const router = express.Router();
-
-// TODO Validate iOS vendor ids
-const UUID_VALIDATE_IOS = /[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}/;
 
 let serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS); 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://campus-density.firebaseio.com"
+  databaseURL: process.env.FIREBASE_URL
 });
 
 export async function authenticated(req, res, next){
   if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-    res.status(403).send('Unauthorized'); 
+    res.status(401).send('Unauthorized'); 
     return; 
   }
   try {
@@ -47,7 +36,7 @@ export async function authenticated(req, res, next){
     next();
   }
   catch {
-    res.status(403).send('Unauthorized: Invalid ID'); 
+    res.status(403).send('Unauthorized: Invalid token ID'); 
   }
 }
 
