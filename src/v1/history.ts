407ef8eb2588258179827203151d874c5bf2a298
Benjamin Shen <bfs45@cornell.edu>
2020-06-27 15:26:43 -0400

    Add mask to hide closed hours in historical data

--- src/v1/history.ts
+++ src/v1/history.ts
@@ -12,34 +12,37 @@ moment.tz.setDefault("America/New_York");
 
 let data = null;
 
-function format(analysis) {
-  const formattedHours = () => ({
-    "0": -1,
-    "1": -1,
-    "2": -1,
-    "3": -1,
-    "4": -1,
-    "5": -1,
-    "6": -1,
-    "7": -1,
-    "8": -1,
-    "9": -1,
-    "10": -1,
-    "11": -1,
-    "12": -1,
-    "13": -1,
-    "14": -1,
-    "15": -1,
-    "16": -1,
-    "17": -1,
-    "18": -1,
-    "19": -1,
-    "20": -1,
-    "21": -1,
-    "22": -1,
-    "23": -1,
-  });
+const range = (start, end, length = end - start + 1) =>
+  Array.from({ length }, (_, i) => start + i);
 
+const formattedHours = () => ({
+  "0": -1,
+  "1": -1,
+  "2": -1,
+  "3": -1,
+  "4": -1,
+  "5": -1,
+  "6": -1,
+  "7": -1,
+  "8": -1,
+  "9": -1,
+  "10": -1,
+  "11": -1,
+  "12": -1,
+  "13": -1,
+  "14": -1,
+  "15": -1,
+  "16": -1,
+  "17": -1,
+  "18": -1,
+  "19": -1,
+  "20": -1,
+  "21": -1,
+  "22": -1,
+  "23": -1,
+});
+
+function format(analysis) {
   return analysis.map(({ id, history }) => {
     const dailyHours = {};
     Object.entries(history).forEach(([k, v]) => {
@@ -64,15 +67,13 @@ function historicalData(id, mask) {
   if (id) {
     const d = formatId(data, id);
     if (d) {
-      console.log(moment());
       Object.entries(d.hours).forEach(([k, v]) => {
-        d.hours[k] = Object.assign(d.hours[k], mask);
+        d.hours[k] = Object.assign(v, mask[k]);
       });
       return JSON.stringify([d]);
-    } else {
-      return "{}";
     }
   }
+  throw new Error("Invalid ID");
 }
 
 import Datastore = require("@google-cloud/datastore");
@@ -90,7 +91,34 @@ export default function routes(redis?: Redis, credentials?) {
     cache(historicalDataKey, redis),
     asyncify(async (req: express.Request, res: express.Response) => {
       try {
-        const mask = {};
+        const facilityHours = await db.facilityHours(
+          req.query.id,
+          moment().format("YYYY-MM-DD"),
+          moment().add(7, "d").format("YYYY-MM-DD")
+        );
+        const results = facilityHours.map((v) => v.result)[0];
+
+        const mask = {
+          SUN: formattedHours(),
+          MON: formattedHours(),
+          TUE: formattedHours(),
+          WED: formattedHours(),
+          THU: formattedHours(),
+          FRI: formattedHours(),
+          SAT: formattedHours(),
+        };
+        results.hours.forEach((element) => {
+          const hours = element.dailyHours;
+          const start = moment.unix(hours.startTimestamp);
+          const end = moment.unix(hours.endTimestamp);
+          const day = start.format("ddd").toUpperCase();
+
+          const startHour = start.hour();
+          const endHour = end.hour();
+          for (const x of range(startHour, endHour)) {
+            delete mask[day][x];
+          }
+        });
 
         const data = historicalData(req.query.id, mask);
 
@@ -100,6 +128,7 @@ export default function routes(redis?: Redis, credentials?) {
 
         res.status(200).send(data);
       } catch (err) {
+        console.log(err);
         // TODO Send actual error codes based on errors. (this applies to all routes)
         res.status(400).send(err);
       }
