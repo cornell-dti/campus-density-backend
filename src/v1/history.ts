b54d241e16e9eab7f6fb968686b30aac3348a091
Benjamin Shen <bfs45@cornell.edu>
2020-07-05 16:29:07 -0400

    Allow for no id in the query string (#34)
    * Update moment usage; allow functions to take ISO-formatted dates
    
    * Fix linter issues
    
    * Restructure /historicalData to use routes
    
    * Add mask to hide closed hours in historical data
    
    * Remove initial formatting
    
    * Fix linter error
    
    * Add back authentication
    
    * Minor moment fix
    
    * Allow for no id in the query string
    
    * Fix linter errors

--- src/v1/history.ts
+++ src/v1/history.ts
@@ -1,73 +1,100 @@
-import * as express from "express";
-import { Redis } from "ioredis";
-import { analysis } from "./data/historicalAnalysis";
-import { FacilityDB } from "./facilities/db";
-import asyncify from "./lib/asyncify";
-import { cache } from "./lib/cache";
+import * as express from 'express';
+import { Redis } from 'ioredis';
+import { analysis } from './data/historicalAnalysis';
+import { FacilityDB } from './facilities/db';
+import asyncify from './lib/asyncify';
+import { cache } from './lib/cache';
 
-import moment = require("moment");
-require("moment-timezone");
+import moment = require('moment');
+require('moment-timezone');
 
-moment.tz.setDefault("America/New_York");
+moment.tz.setDefault('America/New_York');
 
-let data = null;
-
+// inclusive range function
 const range = (start, end, length = end - start + 1) =>
   Array.from({ length }, (_, i) => start + i);
 
 const formattedHours = () => ({
-  "0": -1,
-  "1": -1,
-  "2": -1,
-  "3": -1,
-  "4": -1,
-  "5": -1,
-  "6": -1,
-  "7": -1,
-  "8": -1,
-  "9": -1,
-  "10": -1,
-  "11": -1,
-  "12": -1,
-  "13": -1,
-  "14": -1,
-  "15": -1,
-  "16": -1,
-  "17": -1,
-  "18": -1,
-  "19": -1,
-  "20": -1,
-  "21": -1,
-  "22": -1,
-  "23": -1,
+  '0': -1,
+  '1': -1,
+  '2': -1,
+  '3': -1,
+  '4': -1,
+  '5': -1,
+  '6': -1,
+  '7': -1,
+  '8': -1,
+  '9': -1,
+  '10': -1,
+  '11': -1,
+  '12': -1,
+  '13': -1,
+  '14': -1,
+  '15': -1,
+  '16': -1,
+  '17': -1,
+  '18': -1,
+  '19': -1,
+  '20': -1,
+  '21': -1,
+  '22': -1,
+  '23': -1
 });
 
 function format(analysis) {
   return analysis.map(({ id, history }) => {
     return {
       id,
-      hours: history,
+      hours: history
     };
   });
 }
 
-function historicalData(id, mask) {
-  if (data == null) {
-    data = format(analysis);
+function historicalData(data, id, mask) {
+  const d = data.find(v => v.id === id);
+  if (d) {
+    Object.entries(d.hours).forEach(([k, v]) => {
+      d.hours[k] = Object.assign(v, mask[k]);
+    });
+    return d;
   }
-  if (id) {
-    const d = data.find((v) => v.id === id);
-    if (d) {
-      Object.entries(d.hours).forEach(([k, v]) => {
-        d.hours[k] = Object.assign(v, mask[k]);
-      });
-      return JSON.stringify([d]);
-    }
-  }
-  throw new Error("Invalid ID");
+  throw new Error('Invalid ID');
 }
 
-import Datastore = require("@google-cloud/datastore");
+async function facilityData(data, id, db) {
+  const facilityHours = await db.facilityHours(
+    id,
+    moment().format('YYYY-MM-DD'),
+    moment().add(6, 'd').format('YYYY-MM-DD')
+  );
+  const results = facilityHours.map(v => v.result)[0];
+
+  const mask = {
+    SUN: formattedHours(),
+    MON: formattedHours(),
+    TUE: formattedHours(),
+    WED: formattedHours(),
+    THU: formattedHours(),
+    FRI: formattedHours(),
+    SAT: formattedHours()
+  };
+  results.hours.forEach(element => {
+    const hours = element.dailyHours;
+    const start = moment.unix(hours.startTimestamp);
+    const end = moment.unix(hours.endTimestamp);
+    const day = start.format('ddd').toUpperCase();
+
+    const startHour = start.hour();
+    const endHour = end.hour();
+    for (const x of range(startHour, endHour)) {
+      delete mask[day][x];
+    }
+  });
+
+  return historicalData(data, id, mask);
+}
+
+import Datastore = require('@google-cloud/datastore');
 
 export default function routes(redis?: Redis, credentials?) {
   const datastore = new Datastore(credentials ? { credentials } : undefined);
@@ -75,49 +102,30 @@ export default function routes(redis?: Redis, credentials?) {
 
   const router = express.Router();
 
-  const historicalDataKey = (req) => `/historicalData?${req.query.id || ""}`;
+  const historicalDataKey = req => `/historicalData?${req.query.id || ''}`;
 
   router.get(
-    "/historicalData",
+    '/historicalData',
     cache(historicalDataKey, redis),
     asyncify(async (req: express.Request, res: express.Response) => {
       try {
-        const facilityHours = await db.facilityHours(
-          req.query.id,
-          moment().format("YYYY-MM-DD"),
-          moment().add(6, "d").format("YYYY-MM-DD")
-        );
-        const results = facilityHours.map((v) => v.result)[0];
-
-        const mask = {
-          SUN: formattedHours(),
-          MON: formattedHours(),
-          TUE: formattedHours(),
-          WED: formattedHours(),
-          THU: formattedHours(),
-          FRI: formattedHours(),
-          SAT: formattedHours(),
-        };
-        results.hours.forEach((element) => {
-          const hours = element.dailyHours;
-          const start = moment.unix(hours.startTimestamp);
-          const end = moment.unix(hours.endTimestamp);
-          const day = start.format("ddd").toUpperCase();
-
-          const startHour = start.hour();
-          const endHour = end.hour();
-          for (const x of range(startHour, endHour)) {
-            delete mask[day][x];
-          }
-        });
-
-        const data = historicalData(req.query.id, mask);
-
-        if (redis) {
-          redis.setex(historicalDataKey(req), 60 * 10, data);
+        let result;
+        const data = format(analysis);
+        const {id} = req.query;
+        if (id) {
+          result = [await facilityData(data, id, db)];
+        } else {
+          await Promise.all(
+            data.map(async facility => facilityData(data, facility.id, db))
+          );
+          result = data;
         }
 
-        res.status(200).send(data);
+        if (redis) {
+          redis.setex(historicalDataKey(req), 60 * 10, result);
+        }
+
+        res.status(200).send(result);
       } catch (err) {
         // TODO Send actual error codes based on errors. (this applies to all routes)
         res.status(400).send(err);
